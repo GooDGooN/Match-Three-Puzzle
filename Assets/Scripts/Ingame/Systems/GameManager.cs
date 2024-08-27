190bf51f942187ab4a891b58bff2075dda08deb7
GooDGooN <qwe91911@gmail.com>
2024-08-27 15:02:43 +0900

    테스트를 위해 필드를 7x7로 변경

--- Assets/Scripts/Ingame/Systems/GameManager.cs
+++ Assets/Scripts/Ingame/Systems/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static readonly Vector3Int PieceFieldSize = new Vector3Int(10, 10);
+    public static readonly Vector3Int PieceFieldSize = new Vector3Int(7, 7);
     public const int PieceSize = 36;
     public const int PieceTypeAmount = 7;
     void Start()
