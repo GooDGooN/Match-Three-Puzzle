5bb13c88965731ba1095ab2f6c37c834c16c4d0c
GooDGooN <qwe91911@gmail.com>
2024-08-28 17:45:55 +0900

    FieldInfo 추가

--- Assets/Scripts/Ingame/PuzzlePiece/PuzzlePieceManager.cs
+++ Assets/Scripts/Ingame/PuzzlePiece/PuzzlePieceManager.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class PuzzlePieceManager : MonoBehaviour
 {
+    public struct Field 
+    {
+        public int Width;
+        public int Height;
+        public Vector3Int Size;
+
+        public Field(int width, int height)
+        {
+            Size = new Vector3Int(width, height);
+            Width = width; 
+            Height = height;
+        }
+
+        public static Vector3Int operator/ (Field a, int b)
+        {
+            return a.Size / b;
+        }
+    }
+
     public PuzzlePiece[,] PieceField;
     public List<PuzzlePiece> PieceList;
     public GameObject PieceContainer;
 
     public readonly Vector3Int PieceFieldSize = new Vector3Int(7, 7);
     public readonly int PieceSize = 36;
+
+    public readonly Field FieldInfo = new Field(7, 7);
 
     private void Awake()
     {
