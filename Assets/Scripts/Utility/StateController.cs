0a52ecc724edd046b6a928e51358b7ccfe36b408
GooDGooN <qwe91911@gmail.com>
2024-09-20 17:39:43 +0900

    StateController로 이름변경, 부분 수정

---
+++ Assets/Scripts/Utility/StateController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class StateController<T>
+{
+    private Dictionary<Type, BaseFSM<T>> stateContainer = new();
+    private T myTarget;
+    public BaseFSM<T> CurrentState;
+
+    public StateController(T target)
+    {
+        myTarget = target;
+    }
+
+    public void ChangeState<ST>() where ST : BaseFSM<T>, new()
+    {
+        CurrentState?.StateExit();
+        if (!stateContainer.TryGetValue(typeof(ST), out var state))
+        {
+            state = new ST();
+            stateContainer.Add(typeof(ST), state);
+            state.self = myTarget;
+            state.stateManager = this;
+        }
+        CurrentState = state;
+        CurrentState.StateEnter();
+    }
+}
+
+public class BaseFSM<T>
+{
+    public T self;
+    public StateController<T> stateManager;
+    public virtual void StateEnter() { }
+    public virtual void StateExit() { }
+    public virtual void StateFixedUpdate() { }
+    public virtual void StateUpdate() { }
+}
