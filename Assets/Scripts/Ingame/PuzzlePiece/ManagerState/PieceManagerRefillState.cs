f775200cc7ed5d778fd669665241630c492fe122
GooDGooN <qwe91911@gmail.com>
2024-09-23 19:47:47 +0900

    필드 빈공간의 피스가 바로 재생성 되는 것이 아닌 0.5초 딜레이 후 생성되도록 변경

--- Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerRefillState.cs
+++ Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerRefillState.cs
@@ -2,63 +2,15 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public partial class PuzzlePieceManager
 {
     public class PieceManagerRefillState : BaseFSM<PuzzlePieceManager>
     {
+        private float delay;
         public override void StateEnter()
         {
-            for (int ix = 0; ix < self.PieceField.Length; ix++)
-            {
-                var col = self.PieceField[ix];
-                if (col.Count < self.FieldInfo.Height)
-                {
-                    for (int iy = 0; iy < col.Count; iy++)
-                    {
-                        var targetPiece = self.PieceField[ix][iy];
-                        if (targetPiece.MyIndex.Item2 > iy)
-                        {
-                            self.RepositionPiece(targetPiece, (ix, iy));
-                            self.repositionedPieceQueue.Enqueue(targetPiece);
-                        }
-                    }
-
-                    while (col.Count != self.FieldInfo.Height)
-                    {
-                        var newPiece = GetUseablePiece();
-                        newPiece.MyType = Utility.PickRandom(Utility.GetEnumArray(PieceType.None));
-                        newPiece.MyIndex = (ix, col.Count);
-                        self.PieceField[ix].Add(newPiece);
-
-                        var pos = self.GetPiecePosition(newPiece.MyIndex) + (Vector3Int.up * 300);
-                        newPiece.transform.position = pos;
-
-                        if (col.Count < self.FieldInfo.Height)
-                        {
-                            self.RepositionPiece(newPiece, newPiece.MyIndex);
-                        }
-                        else
-                        {
-                            self.RepositionPiece(newPiece, newPiece.MyIndex, ChangeToMatchable);
-                        }
-                        self.repositionedPieceQueue.Enqueue(newPiece);
-                    }
-
-                    PuzzlePiece GetUseablePiece()
-                    {
-                        foreach (var piece in self.PieceList)
-                        {
-                            if (piece.MyIndex == (-1, -1))
-                            {
-                                return piece;
-                            }
-                        }
-                        return null;
-                    }
-                }
-            }
+            delay = 0.5f;
         }
 
         public override void StateExit()
@@ -67,6 +19,47 @@ public partial class PuzzlePieceManager
 
         public override void StateFixedUpdate()
         {
+            delay -= Time.deltaTime;
+            if (delay < 0.0f)
+            {
+                for (int ix = 0; ix < self.PieceField.Length; ix++)
+                {
+                    var col = self.PieceField[ix];
+                    if (col.Count < self.FieldInfo.Height)
+                    {
+                        for (int iy = 0; iy < col.Count; iy++)
+                        {
+                            var targetPiece = self.PieceField[ix][iy];
+                            if (targetPiece.MyIndex.Item2 > iy)
+                            {
+                                self.RepositionPiece(targetPiece, (ix, iy));
+                                self.repositionedPieceQueue.Enqueue(targetPiece);
+                            }
+                        }
+
+                        while (col.Count != self.FieldInfo.Height)
+                        {
+                            var newPiece = self.GetUseablePiece();
+                            newPiece.MyType = Utility.PickRandom(Utility.GetEnumArray(PieceType.None));
+                            newPiece.MyIndex = (ix, col.Count);
+                            self.PieceField[ix].Add(newPiece);
+
+                            var pos = self.GetPiecePosition(newPiece.MyIndex) + (Vector3Int.up * 300);
+                            newPiece.transform.position = pos;
+
+                            if (col.Count < self.FieldInfo.Height)
+                            {
+                                self.RepositionPiece(newPiece, newPiece.MyIndex);
+                            }
+                            else
+                            {
+                                self.RepositionPiece(newPiece, newPiece.MyIndex, ChangeToMatchable);
+                            }
+                            self.repositionedPieceQueue.Enqueue(newPiece);
+                        }
+                    }
+                }
+            }
         }
 
         public override void StateUpdate()
