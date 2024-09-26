bc13460b03629c6b9dad23f5015349c9eabd2564
GooDGooN <qwe91911@gmail.com>
2024-09-26 15:29:12 +0900

    제일 많이 매치할 수 있는 피스를 힌트에 저장하도록 설정

--- Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerFindMatchableState.cs
+++ Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerFindMatchableState.cs
@@ -7,50 +7,89 @@ public partial class PuzzlePieceManager
 {
     public class PieceManagerFindMatchableState : BaseFSM<PuzzlePieceManager>
     {
+        private List<PuzzlePiece> tempPuzzlePieceList = new();
         public override void StateEnter()
         {
             self.HintPieceList.Clear();
             foreach (var piece in self.PieceList)
             {
-                foreach(var dir in Utility.Get4DirTuples())
+                tempPuzzlePieceList.Clear();
+                foreach (var dir in Utility.Get4DirTuples())
                 {
                     var rightMatrix3x3 = new PieceMatrix3x3();
-                    var selectPieceList = new List<PuzzlePiece>();
-                    for (int mx = 0; mx < 3; mx++)
+                    var blockedCount = 0;
+                    for (int i = 0; i < rightMatrix3x3.Value.Length; i++)
                     {
-                        for (int my = 0; my < 3; my++)
-                        {
-                            /*
-                             * tx = (mx + 1) * cos(dir) - (my - 1) * sin(dir)
-                             * ty = (mx + 1) * sin(dir) + (my - 1) * cos(dir)
-                             */
-                            var tx = piece.MyIndex.Item1 + ((mx + 1) * dir.Item1) - ((my - 1) * dir.Item2);
-                            var ty = piece.MyIndex.Item2 + ((mx + 1) * dir.Item2) + ((my - 1) * dir.Item1);
+                        /*
+                        * tx = (mx + 1) * cos(dir) - (my - 1) * sin(dir)
+                        * ty = (mx + 1) * sin(dir) + (my - 1) * cos(dir)
+                        */
+                        var mx = (i % 3);
+                        var my = (i / 3);
+                        var tx = piece.MyIndex.Item1 + ((mx + 1) * dir.Item1) - ((my - 1) * dir.Item2);
+                        var ty = piece.MyIndex.Item2 + ((mx + 1) * dir.Item2) + ((my - 1) * dir.Item1);
 
-                            if (self.IsPlaceAreExist(tx, ty))
+                        if (self.IsPlaceAreExist(tx, ty))
+                        {
+                            if (self.PieceField[tx][ty].MyType == piece.MyType)
                             {
-                                if (self.PieceField[tx][ty].MyType == piece.MyType)
-                                {
-                                    selectPieceList.Add(self.PieceField[tx][ty]);
-                                    rightMatrix3x3[mx, my] = 1;
-                                }
+                                rightMatrix3x3[mx, my] = 1;
+                                blockedCount = 0;
+                            }
+                        }
+                        else
+                        {
+                            blockedCount++;
+                            if(blockedCount > 3)
+                            {
+                                break;
                             }
                         }
                     }
-                    if(MatchablePattern.CheckEqual(rightMatrix3x3))
+
+                    var matrixIndex = MatchablePattern.GetEqualMatrixIndex(rightMatrix3x3);
+                    if (matrixIndex != -1)
                     {
-                        var posTuple = (piece.MyIndex.Item1 + dir.Item1, piece.MyIndex.Item2 + dir.Item2);
-                        var testType = piece.MyType;
-                        self.HintPieceList.AddRange(self.GetMatchablePieces(posTuple, testType));
-                        self.HintPieceList.Remove(null);
-                        Debug.Log($"matchable is {piece.MyIndex} Piece");
-                        stateManager.ChangeState<PieceManagerIdleState>();
-                        return;
+                        // save hint
+                        var posTupleSave = piece.MyIndex;
+                        var testPosTuple = piece.MyIndex;
+                        switch(matrixIndex)
+                        {
+                            case 1:
+                                testPosTuple.Item1 += -dir.Item2;
+                                testPosTuple.Item2 += dir.Item1;
+                                break;
+                            case 2:
+                                testPosTuple.Item1 += dir.Item2;
+                                testPosTuple.Item2 += -dir.Item1;
+                                break;
+                            default:
+                                testPosTuple.Item1 += dir.Item1;
+                                testPosTuple.Item2 += dir.Item2;
+                                break;
+                        }
+
+                        piece.MyIndex = testPosTuple;
+                        tempPuzzlePieceList.AddRange(self.GetMatchablePieces(piece));
+                        piece.MyIndex = posTupleSave;
                     }
+                }
+                tempPuzzlePieceList = tempPuzzlePieceList.Distinct().ToList();
+                if (self.HintPieceList.Count < tempPuzzlePieceList.Count)
+                {
+                    self.HintPieceList = tempPuzzlePieceList.ToList();
                 }
             }
 
-            #region Clear all field cause there is no matchable
+            // Some piece is matchable
+            if(self.HintPieceList.Count > 0)
+            {
+                Debug.Log($"matchable is {self.HintPieceList.Last().MyIndex} Piece");
+                stateManager.ChangeState<PieceManagerIdleState>();
+                return;
+            }
+
+            // Clear all field cause there is no matchable
             Debug.Log("there is nothing to do!!");
             for (int ix = 0; ix < self.FieldInfo.Width; ix++)
             {
@@ -60,8 +99,6 @@ public partial class PuzzlePieceManager
                 }
             }
             stateManager.ChangeState<PieceManagerGeneratePieceState>();
-            #endregion
-
         }
 
         public override void StateExit()
