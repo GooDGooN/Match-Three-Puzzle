74de080c0484b9b62a2c404410968c57a2099c29
GooDGooN <qwe91911@gmail.com>
2024-10-01 17:33:03 +0900

    위치를 얻기 위한 확장메소드 GetIndex추가

--- Assets/Scripts/Utility/ExtentionMethod.cs
+++ Assets/Scripts/Utility/ExtentionMethod.cs
@@ -2,8 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static PuzzlePieceManager;
 
-public class ExtentionMethod : MonoBehaviour
+public static class ExtentionMethod
 {
-
+    public static int GetIndex(this PuzzlePiece[] pieces, PuzzlePiece target)
+    {
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            if (pieces[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
