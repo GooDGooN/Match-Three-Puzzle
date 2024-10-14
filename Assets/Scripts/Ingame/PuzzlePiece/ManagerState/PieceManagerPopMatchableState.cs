0298687d35d40455c75f5f29d4b9c27038eb9328
GooDGooN <qwe91911@gmail.com>
2024-10-14 18:28:03 +0900

    기존 폭탄 작동 상태의 내용을 수정하여 검사 및 피스 터트리는 상태에 이식

--- Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerPopMatchableState.cs
+++ Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerPopMatchableState.cs
@@ -119,8 +119,136 @@ public partial class PuzzlePieceManager
         }
     }
 
+    private void StartActiveBomb(PuzzlePiece target)
+    {
+        switch(target.MySubType)
         {
+            case PieceSubType.Vbomb:
+                StartCoroutine(ActiveVerticalBomb(target));
+                break;
+            case PieceSubType.Hbomb:
+                StartCoroutine(ActiveHorizontalBomb(target));
+                break;
+            case PieceSubType.CrossBomb:
+                StartCoroutine(ActiveVerticalBomb(target));
+                StartCoroutine(ActiveHorizontalBomb(target));
+                break;
+        }
+        stateChangeDelay = 0.16f;
+        target.RemoveSelf();
+    }
+
+    private IEnumerator ActiveHorizontalBomb(PuzzlePiece targetPiece)
+    {
+        var targetTuple = targetPiece.MyIndex;
+
+        var increasedTuple = (targetTuple.Item1 + 1, targetTuple.Item2);
+        var decreasedTuple = (targetTuple.Item1 - 1, targetTuple.Item2);
+        var repeatTime = targetTuple.Item1 > FieldInfo.Width / 2 ? targetTuple.Item1 : FieldInfo.Width - targetTuple.Item1;
+        var delayTime = 0.15f / repeatTime;
+        while (repeatTime-- > 0)
+        {
+            // check right
+            if (IsPlaceAreExist(increasedTuple) && !IsPlaceEmpty(increasedTuple))
+            {
+                var target = MyPieceField[increasedTuple.Item1, increasedTuple.Item2];
+                if (target.MyIndex == increasedTuple)
+                {
+                    var targetSubType = target.MySubType;
+                    if (targetSubType == PieceSubType.Vbomb)
+                    {
+                        if (target.MyIndex != (-1, -1))
+                        {
+                            StartActiveBomb(target);
+                        }
+                    }
+                    else
+                    {
+                        target.RemoveSelf();
+                    }
+                }
+                increasedTuple = (increasedTuple.Item1 + 1, increasedTuple.Item2);
+            }
+
+            // check left
+            if (IsPlaceAreExist(decreasedTuple) && !IsPlaceEmpty(decreasedTuple))
+            {
+                var target = MyPieceField[decreasedTuple.Item1, decreasedTuple.Item2];
+                if (target.MyIndex == decreasedTuple)
+                {
+                    var targetSubType = target.MySubType;
+                    if (targetSubType == PieceSubType.Vbomb)
+                    {
+                        if (target.MyIndex != (-1, -1))
+                        {
+                            StartActiveBomb(target);
+                        }
+                    }
+                    else
+                    {
+                        target.RemoveSelf();
+                    }
+                }
+                decreasedTuple = (decreasedTuple.Item1 - 1, decreasedTuple.Item2);
+            }
+            yield return new WaitForSeconds(delayTime);
         }
     }
 
+    private IEnumerator ActiveVerticalBomb(PuzzlePiece targetPiece)
+    {
+        var targetTuple = targetPiece.MyIndex;
+
+        var increasedTuple = (targetTuple.Item1, targetTuple.Item2);
+        var decreasedTuple = (targetTuple.Item1, targetTuple.Item2 - 1);
+        var repeatTime = targetTuple.Item2 > FieldInfo.Height / 2 ? targetTuple.Item2 : FieldInfo.Height - targetTuple.Item2;
+        var delayTime = 0.15f / repeatTime;
+        while (repeatTime-- > 0)
+        {
+            // check up
+            if (IsPlaceAreExist(increasedTuple) && !IsPlaceEmpty(increasedTuple))
+            {
+                var target = MyPieceField[increasedTuple.Item1, increasedTuple.Item2];
+                if (target.MyIndex == increasedTuple)
+                {
+                    var targetSubType = target.MySubType;
+                    if (targetSubType == PieceSubType.Hbomb)
+                    {
+                        if (target.MyIndex != (-1, -1))
+                        {
+                            StartActiveBomb(target);
+                        }
+                    }
+                    else
+                    {
+                        target.RemoveSelf();
+                    }
+                }
+            }
+
+            // check down
+            if (IsPlaceAreExist(decreasedTuple) && !IsPlaceEmpty(decreasedTuple))
+            {
+                var target = MyPieceField[decreasedTuple.Item1, decreasedTuple.Item2];
+                if (target.MyIndex == decreasedTuple)
+                {
+                    var targetSubType = target.MySubType;
+                    if (targetSubType == PieceSubType.Hbomb)
+                    {
+                        if (target.MyIndex != (-1, -1))
+                        {
+                            StartActiveBomb(target);
+                        }
+                    }
+                    else
+                    {
+                        target.RemoveSelf();
+                    }
+                }
+                decreasedTuple = (decreasedTuple.Item1, decreasedTuple.Item2 - 1);
+                increasedTuple = (decreasedTuple.Item1, decreasedTuple.Item2 + 1);
+            }
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
 }
