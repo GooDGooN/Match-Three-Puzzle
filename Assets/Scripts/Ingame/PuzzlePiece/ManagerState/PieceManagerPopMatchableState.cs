2723e9488b29bf34b4ca3b80567ecdfacaeac553
GooDGooN <qwe91911@gmail.com>
2024-10-14 18:27:03 +0900

    검사 및 피스 터트리는 상태에서 새로운 폭탄 규칙에 맞게 폭탄이 생성되도록 변경

--- Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerPopMatchableState.cs
+++ Assets/Scripts/Ingame/PuzzlePiece/ManagerState/PieceManagerPopMatchableState.cs
@@ -1,54 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public partial class PuzzlePieceManager
 {
+    private List<PuzzlePiece> removeTargetList = new();
+    private float stateChangeDelay;
     public class PieceManagerPopMatchableState : BaseFSM<PuzzlePieceManager>
     {
-        private float delay;
         private bool isRefill;
-        private Queue<PuzzlePiece> removePieceQueue;
+        private List<PuzzlePiece[]> removePiecesList;
         public override void StateEnter()
         {
-            removePieceQueue = new();
-            delay = 0.1f;
+            removePiecesList = new();
+            self.stateChangeDelay = 0.0f;
             isRefill = false;
+
+            while (self.repositionedPieceQueue.Count > 0)
+            {
+                var target = self.repositionedPieceQueue.Dequeue();
+                var matchables = self.GetMatchablePieces(target);
+                if (matchables.Length > 0)
+                {
+                    removePiecesList.Add(matchables);
+                }
+            }
+
+            removePiecesList = removePiecesList.OrderBy(arr => arr.Length).ToList();
+            PuzzlePiece specialPiece = null;
+
+            foreach (var matchables in removePiecesList)
+            {
+
+                specialPiece = null;
+                var matchLength = matchables.Length;
+                if (matchLength > 3 && matchables.Count(arr => arr.MyIndex == (-1, -1)) == 0)
+                {
+                    // set the special piece
+                    if (matchables.Contains(self.selectedPuzzlePiece))
+                    {
+                        specialPiece = self.selectedPuzzlePiece;
+                    }
+                    else if (matchables.Contains(self.swapTargetPuzzlePiece))
+                    {
+                        specialPiece = self.swapTargetPuzzlePiece;
+                    }
+                    else
+                    {
+                        specialPiece = matchables[Random.Range(0, matchables.Length)];
+                    }
+
+                    // set special piece type
+                    if (matchLength >= 6)
+                    {
+                        specialPiece.TargetChangeType = PieceType.Rainbow;
+                    }
+                    else if (matchLength == 5)
+                    {
+                        if (matchables.Count(piece => piece.MyIndex.Item1 == matchables[0].MyIndex.Item1) == 5 ||
+                            matchables.Count(piece => piece.MyIndex.Item2 == matchables[0].MyIndex.Item2) == 5)
+                        {
+                            specialPiece.TargetChangeType = PieceType.Rainbow;
+                        }
+                        else
+                        {
+                            specialPiece.TargetChangeSubType = PieceSubType.CrossBomb;
+                        }
+                    }
+                    else if (matchLength == 4)
+                    {
+                        if (matchables[0].MyIndex.Item1 == matchables[1].MyIndex.Item1)
+                        {
+                            specialPiece.TargetChangeSubType = PieceSubType.Hbomb;
+                        }
+                        else if (matchables[0].MyIndex.Item2 == matchables[1].MyIndex.Item2)
+                        {
+                            specialPiece.TargetChangeSubType = PieceSubType.Vbomb;
+                        }
+                    }
+                    specialPiece.TargetChangeType = specialPiece.MyType;
+                }
+
+                specialPiece?.MyAnimator.SetTrigger("ChangeType");
+                foreach (var piece in matchables)
+                {
+                    if (piece != specialPiece && piece.MyIndex != (-1, -1))
+                    {
+                        isRefill = true;
+                        piece.RemoveSelf();
+                    }
+                }
+            }
         }
 
         public override void StateExit()
         {
-
+            self.swapTargetPuzzlePiece = null;
+            self.selectedPuzzlePiece = null;
         }
 
         public override void StateFixedUpdate()
         {
-            delay -= Time.deltaTime;
-            if(delay < 0.0f)
+        }
+
+        public override void StateUpdate()
+        {
+            self.stateChangeDelay -= Time.deltaTime;
+            if (self.stateChangeDelay < 0.0f)
             {
-                while (self.repositionedPieceQueue.Count > 0)
-                {
-                    var target = self.repositionedPieceQueue.Dequeue();
-                    var matchables = self.GetMatchablePieces(target);
-                    if (matchables.Length > 0)
-                    {
-                        isRefill = true;
-                        foreach (var piece in matchables)
-                        {
-                            removePieceQueue.Enqueue(piece);
-                        }
-                    }
-                }
-
-                while(removePieceQueue.Count > 0)
-                {
-                    var piece = removePieceQueue.Dequeue();
-                    if (piece.MyIndex != (-1, -1))
-                    {
-                        piece.RemoveSelf();
-                    }
-                }
-
                 if (isRefill)
                 {
                     stateManager.ChangeState<PieceManagerRefillState>();
@@ -57,8 +117,8 @@ public partial class PuzzlePieceManager
                 stateManager.ChangeState<PieceManagerFindMatchableState>();
             }
         }
+    }
 
-        public override void StateUpdate()
         {
         }
     }
