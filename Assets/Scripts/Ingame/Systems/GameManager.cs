05fbb8ebe15297619d5e0a619f0de3afee47d74e
GooDGooN <qwe91911@gmail.com>
2025-01-08 08:19:18 +0900

    기본 작동하는 타이머 추가

--- Assets/Scripts/Ingame/Systems/GameManager.cs
+++ Assets/Scripts/Ingame/Systems/GameManager.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -12,6 +14,9 @@ public class GameManager : Singleton<GameManager>
     public int TotalScore = 0;
     public int Combo = 0;
 
+    public float TimeLimitValue = 0;
+    public GameObject TimeLimitBarImageObject;
+
     protected override void Awake()
     {
         base.Awake();
@@ -20,7 +25,7 @@ public class GameManager : Singleton<GameManager>
 
     private void Start()
     {
-        
+        TimeLimitValue = 1.0f;
     }
 
     // Update is called once per frame
@@ -30,6 +35,34 @@ public class GameManager : Singleton<GameManager>
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene(0);
+        }
+
+        if (TimeLimitValue > 0)
+        {
+            TimeLimitValue -= Time.deltaTime / 60.0f;
+            var rectTransform = TimeLimitBarImageObject.GetComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(320.0f * TimeLimitValue, 32.0f);
+
+            var targetColor = new Color32();
+            targetColor.a = 255;
+            targetColor.b = 0;
+            if (TimeLimitValue > 0.7f)
+            {
+                targetColor.r = 27;
+                targetColor.g = 205;
+            } 
+            else if (TimeLimitValue > 0.3f)
+            {
+                targetColor.r = 180;
+                targetColor.g = 180;
+            } 
+            else
+            {
+                targetColor.r = 205;
+                targetColor.g = 27;
+            }
+
+            TimeLimitBarImageObject.GetComponent<Image>().color = targetColor;
         }
     }
 
